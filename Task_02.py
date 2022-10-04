5e1c4fcee5323978c846136665616a595d568f7d
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-10-04 23:39:26 +0700

    .\Task_02.py

---
+++ Task_02.py
@@ -0,0 +1,6 @@
+# Напишите программу для проверки истинности утверждения
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+
+values = False, True
+print(all(not(x or y or z) == (not x and not y and not z) for x in values for y in values for z in values))
